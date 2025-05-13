0fe1a1f0378b681b85c4f8177e44cef715a4dc42
AntonioRomaniello <romaniellantonio@gmail.com>
2025-05-13 15:14:10 +0200

    Prova del nuovo test

--- 0513.SpringSecurity/src/main/java/corso/java/securitysample/controllers/DataController.java
+++ 0513.SpringSecurity/src/main/java/corso/java/securitysample/controllers/DataController.java
@@ -1,22 +1,31 @@
 package corso.java.securitysample.controllers;
 
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import corso.java.securitysample.entities.UserEntity;
+import corso.java.securitysample.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/test")
+@RequiredArgsConstructor
 public class DataController {
 
-	@GetMapping
-	public ResponseEntity<List<String>> test() {
-		return ResponseEntity.ok( //
-				IntStream.range(0, 100) //
-						.mapToObj(n -> String.format("Stringa %s", n)) //
-						.toList());
-	}
+    private final UserRepository userRepository; // Iniettare il repository degli utenti
+
+    @GetMapping
+    public ResponseEntity<List<String>> test() {
+        // Recupera tutti gli utenti dal database e crea una lista di stringhe con username e ruolo
+        List<String> usersWithRoles = userRepository.findAll().stream()
+                .map(user -> String.format("Utente: %s, Ruolo: %s", user.getUsername(), user.getRole()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(usersWithRoles);
+    }
 }
